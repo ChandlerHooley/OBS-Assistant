b1e359a8c87eff14bf85ae1e4b23b7d8e2420dd5
ChandlerHooley <chandler@thehooleyfamily.com>
2023-03-30 16:17:31 -0600

    Add files via upload
    NameTestPass prompts for a number then plays the corresponding song.

---
+++ NameTestPass.ps1
@@ -0,0 +1,18 @@
+﻿# Ask user for video name
+$video_name = Read-Host "Enter video name"
+
+# Get video file path
+$video_path = "C:\Users\chand\Videos\OBS_V\LCG_Hymnal\TV\$video_name.mp4"
+
+# Check if file exists
+if (Test-Path $video_path) {
+    # Get VLC executable path
+    $vlc_path = "C:\Program Files\VideoLAN\VLC\vlc.exe"
+
+    # Start VLC with video file
+    Start-Process -FilePath $vlc_path -ArgumentList $video_path
+}
+else {
+    # Show error
+    Write-Error "Video not found"
+}
